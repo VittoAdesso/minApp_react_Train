c0f1b32821bf2e103776864681e43b4d9ce7afe7
Vittorio Adesso <adessovittorio8@gmail.com>
2022-07-22 14:00:52 +0200

    added emojiPicker app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import TodoApp from './components/todoAPP/todoApp';
-import WeatherApp from './components/weatherApp/weatherApp';
+// import WeatherApp from './components/weatherApp/weatherApp';
+import EmojiPickerInput from './components/emojis/emojiPicker/emojiPickerInput';
+
+
 
 function App() {
   return (
@@ -8,7 +11,8 @@ function App() {
     {/* TODO: Make router links to have spa with projects */}
       <TodoApp></TodoApp>
       <hr></hr>
-      <WeatherApp></WeatherApp>
+      {/* <WeatherApp></WeatherApp> */}
+      <EmojiPickerInput/>
     </div>
   );
 }
